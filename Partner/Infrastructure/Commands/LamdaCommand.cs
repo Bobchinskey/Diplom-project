ae5f46cc99cc06bbeb31b4403fd5e8bae2785ed0
Михаил Александрович Александренко <98051902+Bobchinskey@users.noreply.github.com>
2022-04-06 11:40:51 +0400

    Настройка структуры проекта.

---
+++ Partner/Infrastructure/Commands/LamdaCommand.cs
@@ -0,0 +1,20 @@
+﻿using Partner.Infrastructure.Commands.Base;
+using System;
+
+namespace Partner.Infrastructure.Commands
+{
+    internal class LamdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LamdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parametr) => _CanExecute?.Invoke(parametr) ?? true;
+
+        public override void Execute(object parametr) => _Execute(parametr);
+    }
+}
