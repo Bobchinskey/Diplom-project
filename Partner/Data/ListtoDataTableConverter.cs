7e9b8b62684c5a900cfa29b1da2cb019818dbe9a
Bobchinskey <miha.alexandrenko@gmail.com>
2022-05-20 23:25:18 +0400

    Проверка 2

--- Partner/Data/ListtoDataTableConverter.cs
+++ Partner/Data/ListtoDataTableConverter.cs
@@ -9,11 +9,9 @@ namespace Partner.Data
         public DataTable ToDataTable<T>(List<T> items)
         {
             DataTable dataTable = new DataTable(typeof(T).Name);
-            //Get all the properties
             PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
             foreach (PropertyInfo prop in Props)
             {
-                //Setting column names as Property names
                 dataTable.Columns.Add(prop.Name);
             }
             foreach (T item in items)
@@ -21,12 +19,10 @@ namespace Partner.Data
                 var values = new object[Props.Length];
                 for (int i = 0; i < Props.Length; i++)
                 {
-                    //inserting property values to datatable rows
                     values[i] = Props[i].GetValue(item, null);
                 }
                 dataTable.Rows.Add(values);
             }
-            //put a breakpoint here and check datatable
             return dataTable;
         }
     }
